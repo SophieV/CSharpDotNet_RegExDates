f5d62befce487ed3c214fa47318e67f3144b83f9
Sophie <sophie@adoptive.co>
2014-12-16 16:37:15 -0500

    combined dates

--- RegEx/DatesRewriter.cs
+++ RegEx/DatesRewriter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -7,36 +8,59 @@ using System.Threading.Tasks;
 
 namespace RegEx
 {
+    // (?:[0]*): ignore "trailing" zeros
+    // (?<month>\d{1,2}) : consider the next one or two digits as the month
+    // \W+: at least one non-alphanumeric character
+    // (\d{4})\d{2}(?<year>\d{2})|(?<year>\d{2}): if year has 4 digits, ignore two and take last two. else take 2. (will fail if only 1)
+    // [a-zA-Z]+ : at least one alphabetical character
+
     public class DatesRewriter
     {
-        public string ConvertDate(string input)
+        public string[] ConvertDate(string input)
         {
-            string converted = null;
+            List<string> converted = new List<string>();
 
-            converted = SingleFullNumericDate(input);
+            converted.AddRange(SingleFullNumericDate(input));
 
-            if (converted == null)
+            if (converted.Count() == 0)
             {
-                converted = SingleMonthAndYearNumericDate(input);
+                converted.AddRange(SingleMonthAndYearsNumericDate(input));
             }
 
-            if (converted == null)
+            if (converted.Count() == 0)
             {
-                converted = SingleYearNumericDate(input);
+                converted.AddRange(SingleMonthAndYearNumericDate(input));
             }
 
-            if (converted == null)
+            if (converted.Count() == 0)
             {
-                converted = YearNumericDashedDateRange(input);
+                converted.AddRange(WrittenMonthAndYear(input));
+            } 
+
+            if (converted.Count() == 0)
+            {
+                converted.AddRange(YearNumericDashedDateRange(input));
+            }
+            
+            if (converted.Count() == 0)
+            {
+                converted.AddRange(YearNumericFullThenSingleDateRange(input));
             }
 
-            return converted;
+            if (converted.Count() == 0)
+            {
+                // is less specific than YearNumericFullThenSingleDateRange
+                converted.AddRange(MultipleYearsRange(input));
+            }
+
+            return converted.ToArray();
         }
 
-        private string YearNumericDashedDateRange(string input)
+        // identifies "2003-now" or "98 - present"
+        private List<string> YearNumericDashedDateRange(string input)
         {
-            string converted = null;
-            string pattern = @"\b(?:\d*)(?<year>\d{2}) \W? \w*\b";
+            List<string> converted = new List<string>();
+            string pattern = @"\b(?:[0]*)(?(\d{4})\d{2}(?<year>\d{2})|(?<year>\d{2}))\W+[a-zA-Z]+\b";
             var regex = new Regex(pattern, RegexOptions.IgnorePatternWhitespace);
             MatchCollection allMatches = regex.Matches(input);
 
@@ -49,38 +73,79 @@ namespace RegEx
                 builder.Append('1');
                 builder.Append('/');
                 builder.Append(match.Groups["year"].Value);
-                converted = builder.ToString();
+                converted.Add(builder.ToString());
             }
 
             return converted;
         }
 
-        private string SingleYearNumericDate(string input)
+        // identifies "2004-8" or "04-8"
+        private List<string> YearNumericFullThenSingleDateRange(string input)
         {
-            string converted = null;
-            string pattern = @"\b(?:\d*)(?<year>\d{2}$)\b";
+            List<string> converted = new List<string>();
+            string pattern = @"\b(?:[0]*)(?(\d{4})\d{2}(?<year1>\d{2})|(?<year1>\d{2}))\W+(?<year2>\d{1})\b";
             var regex = new Regex(pattern, RegexOptions.IgnorePatternWhitespace);
             MatchCollection allMatches = regex.Matches(input);
 
             foreach (Match match in allMatches)
             {
                 StringBuilder builder = new StringBuilder();
+                // default value
+                builder.Append('1');
+                builder.Append('/');
+                // default value
+                builder.Append('1');
+                builder.Append('/');
+                builder.Append(match.Groups["year1"].Value);
+                converted.Add(builder.ToString());
+
+                builder = new StringBuilder();
+                // default value
+                builder.Append('1');
+                builder.Append('/');
+                // default value
+                builder.Append('1');
+                builder.Append('/');
+                // missing digit of year - must be 2
+                builder.Append('0');
+                builder.Append(match.Groups["year2"].Value);
+                converted.Add(builder.ToString());
+            }
+
+            return converted;
+        }
+
+        // identifies a year, could be just as many as wanted
+        // such as "2002,2003" or "1998-1999-2000"
+        // "19911992" is not recognized though
+        private List<string> MultipleYearsRange(string input)
+        {
+            List<string> converted = new List<string>();
+            string pattern = @"\b(?:[0]*)(?(\d{4})\d{2}(?<year>\d{2})|(?<year>\d{2}))\b";
+            var regex = new Regex(pattern, RegexOptions.IgnorePatternWhitespace);
+            MatchCollection allMatches = regex.Matches(input);
+
+            foreach (Match match in allMatches)
+            {
+                StringBuilder builder = new StringBuilder();
+                // default value
                 builder.Append('1');
                 builder.Append('/');
                 // default value
                 builder.Append('1');
                 builder.Append('/');
                 builder.Append(match.Groups["year"].Value);
-                converted = builder.ToString();
+                converted.Add(builder.ToString());
             }
 
             return converted;
         }
 
-        private string SingleMonthAndYearNumericDate(string input)
+        // identifies "09/1988"
+        private List<string> SingleMonthAndYearNumericDate(string input)
         {
-            string converted = null;
-            string pattern = @"\b(?:[0]?)(?<month>\d{1,2})/(?:\d*)(?<year>\d{2}$)\b";
+            List<string> converted = new List<string>();
+            string pattern = @"\b(?:[0]*)(?<month>\d{1,2})\W+(?:[0]*)(?(\d{4})\d{2}(?<year>\d{2})|(?<year>\d{2}))\b";
             var regex = new Regex(pattern, RegexOptions.IgnorePatternWhitespace);
             MatchCollection allMatches = regex.Matches(input);
 
@@ -93,16 +158,49 @@ namespace RegEx
                 builder.Append('1');
                 builder.Append('/');
                 builder.Append(match.Groups["year"].Value);
-                converted = builder.ToString();
+                converted.Add(builder.ToString());
             }
 
             return converted;
         }
 
-        private string SingleFullNumericDate(string input)
+        // identifies "09/1988-89"
+        private List<string> SingleMonthAndYearsNumericDate(string input)
         {
-            string converted = null;
-            string pattern = @"\b(?:[0]?)(?<month>\d{1,2})\W{1}(?:[0]?)(?<day>\d{1,2})\W{1}(?:\d*)(?<year>\d{2}$)\b";
+            List<string> converted = new List<string>();
+            string pattern = @"\b(?:[0]*)(?<month>\d{1,2})\W+(?:[0]*)(?(\d{4})\d{2}(?<year1>\d{2})|(?<year1>\d{2}))\W+(?:[0]*)(?(\d{4})\d{2}(?<year2>\d{2})|(?<year2>\d{2}))$\b";
+            var regex = new Regex(pattern, RegexOptions.IgnorePatternWhitespace);
+            MatchCollection allMatches = regex.Matches(input);
+
+            foreach (Match match in allMatches)
+            {
+                StringBuilder builder = new StringBuilder();
+                builder.Append(match.Groups["month"].Value);
+                builder.Append('/');
+                // default value
+                builder.Append('1');
+                builder.Append('/');
+                builder.Append(match.Groups["year1"].Value);
+                converted.Add(builder.ToString());
+
+                builder = new StringBuilder();
+                builder.Append(match.Groups["month"].Value);
+                builder.Append('/');
+                // default value
+                builder.Append('1');
+                builder.Append('/');
+                builder.Append(match.Groups["year2"].Value);
+                converted.Add(builder.ToString());
+            }
+
+            return converted;
+        }
+
+        // identifies "04/11/1987" or "5-8-08"
+        private List<string> SingleFullNumericDate(string input)
+        {
+            List<string> converted = new List<string>();
+            string pattern = @"\b(?:[0]*)(?<month>\d{1,2})\W+(?:[0]*)(?<day>\d{1,2})\W+(?:[0]*)(?(\d{4})\d{2}(?<year>\d{2})|(?<year>\d{2}))\b";
             var regex = new Regex(pattern, RegexOptions.IgnorePatternWhitespace);
             MatchCollection allMatches = regex.Matches(input);
 
@@ -114,7 +212,30 @@ namespace RegEx
                 builder.Append(match.Groups["day"].Value);
                 builder.Append('/');
                 builder.Append(match.Groups["year"].Value);
-                converted = builder.ToString();
+                converted.Add(builder.ToString());
+            }
+
+            return converted;
+        }
+
+        // recognizes "June, 2008" as well as "June,2007-September,2010"
+        private List<string> WrittenMonthAndYear(string input)
+        {
+            List<string> converted = new List<string>();
+            string pattern = @"\b(?<month>[a-zA-Z]+)\W+(?:[0]*)(?(\d{4})\d{2}(?<year>\d{2})|(?<year>\d{2}))\b";
+            var regex = new Regex(pattern, RegexOptions.IgnorePatternWhitespace);
+            MatchCollection allMatches = regex.Matches(input);
+
+            foreach (Match match in allMatches)
+            {
+                StringBuilder builder = new StringBuilder();
+                builder.Append(DateTime.ParseExact(match.Groups["month"].Value, "MMMM", CultureInfo.InvariantCulture).Month);
+                builder.Append('/');
+                // default value
+                builder.Append('1');
+                builder.Append('/');
+                builder.Append(match.Groups["year"].Value);
+                converted.Add(builder.ToString());
             }
 
             return converted;
--- RegExTDD/UnitTestDatesRegEx.cs
+++ RegExTDD/UnitTestDatesRegEx.cs
@@ -13,117 +13,234 @@ namespace RegExTDD
         public void SingleFullNumericDate_FullYear_Test()
         {
             string input = "07/01/2001";
-            string expected = "7/1/01";
+            string[] expected = {"7/1/01"};
 
-            Assert.AreEqual(expected, rewriter.ConvertDate(input));
+            CollectionAssert.AreEqual(expected, rewriter.ConvertDate(input));
         }
 
         [TestMethod]
         public void SingleFullNumericDate_3DigitDay_Test()
         {
             string input = "11/019/1997";
-            string expected = "11/19/97";
+            string[] expected = {"11/19/97"};
 
-            Assert.AreEqual(expected, rewriter.ConvertDate(input));
+            CollectionAssert.AreEqual(expected, rewriter.ConvertDate(input));
         }
 
         [TestMethod]
         public void SingleFullNumericDate_ShortYear_Test()
         {
             string input = "01/01/07";
-            string expected = "1/1/07";
+            string[] expected = {"1/1/07"};
 
-            Assert.AreEqual(expected, rewriter.ConvertDate(input));
+            CollectionAssert.AreEqual(expected, rewriter.ConvertDate(input));
         }
 
         [TestMethod]
         public void SingleFullNumericDate_NoDayFullYear_Test()
         {
             string input = "07/2001";
-            string expected = "7/1/01";
+            string[] expected = {"7/1/01"};
 
-            Assert.AreEqual(expected, rewriter.ConvertDate(input));
+           CollectionAssert.AreEqual(expected, rewriter.ConvertDate(input));
         }
 
         [TestMethod]
         public void SingleFullNumericDate_NoDayShortYear_Test()
         {
             string input = "9/96";
-            string expected = "9/1/96";
+            string[] expected = {"9/1/96"};
 
-            Assert.AreEqual(expected, rewriter.ConvertDate(input));
+            CollectionAssert.AreEqual(expected, rewriter.ConvertDate(input));
         }
 
         [TestMethod]
         public void SingleFullNumericDate_FullYearOnly_Test()
         {
             string input = "2001";
-            string expected = "1/1/01";
+            string[] expected = {"1/1/01"};
 
-            Assert.AreEqual(expected, rewriter.ConvertDate(input));
+            CollectionAssert.AreEqual(expected, rewriter.ConvertDate(input));
         }
 
         [TestMethod]
         public void SingleFullNumericDate_FullYearDash_Test()
         {
             string input = "2008 -";
-            string expected = "1/1/08";
+            string[] expected = {"1/1/08"};
 
-            Assert.AreEqual(expected, rewriter.ConvertDate(input));
+            CollectionAssert.AreEqual(expected, rewriter.ConvertDate(input));
         }
 
         [TestMethod]
         public void SingleFullNumericDate_FullYearDashPresent_Test()
         {
             string input = "1998 - present";
-            string expected = "1/1/98";
+            string[] expected = {"1/1/98"};
 
-            Assert.AreEqual(expected, rewriter.ConvertDate(input));
+            CollectionAssert.AreEqual(expected, rewriter.ConvertDate(input));
         }
 
         [TestMethod]
         public void SingleFullNumericDate_FullYearDashPresentNoSpace_Test()
         {
             string input = "2005-present";
-            string expected = "1/1/05";
+            string[] expected = {"1/1/05"};
 
-            Assert.AreEqual(expected, rewriter.ConvertDate(input));
+            CollectionAssert.AreEqual(expected, rewriter.ConvertDate(input));
         }
 
         [TestMethod]
         public void SingleFullNumericDate_FullDateDashed_Test()
         {
             string input = "05-01-2009";
-            string expected = "5/1/09";
+            string[] expected = {"5/1/09"};
 
-            Assert.AreEqual(expected, rewriter.ConvertDate(input));
+            CollectionAssert.AreEqual(expected, rewriter.ConvertDate(input));
+        }
+
+        [TestMethod]
+        public void FullNumericDateRange_FullDateDashed_Test()
+        {
+            string input = "4/1/99-7/1/14";
+            string[] expected = { "4/1/99","7/1/14" };
+
+            CollectionAssert.AreEqual(expected, rewriter.ConvertDate(input));
+        }
+
+        [TestMethod]
+        public void MixMonthYearDateRange_FullDateDashed_Test()
+        {
+            string input = "09/1993-02/94";
+            string[] expected = { "9/1/93", "2/1/94" };
+
+            CollectionAssert.AreEqual(expected, rewriter.ConvertDate(input));
+        }
+
+        [TestMethod]
+        public void CompactMonthYearDateRange_FullDateDashed_Test()
+        {
+            string input = "07/2004-09";
+            string[] expected = { "7/1/04", "7/1/09" };
+
+            CollectionAssert.AreEqual(expected, rewriter.ConvertDate(input));
         }
 
         [TestMethod]
         public void SingleFullNumericDate_FullDateDashedComma_Test()
         {
             string input = "05/22,2007";
-            string expected = "5/22/07";
+            string[] expected = {"5/22/07"};
 
-            Assert.AreEqual(expected, rewriter.ConvertDate(input));
+            CollectionAssert.AreEqual(expected, rewriter.ConvertDate(input));
         }
 
         [TestMethod]
         public void SingleFullNumericDate_FullDateQuote_Test()
         {
             string input = "06'09/1988";
-            string expected = "6/9/88";
+            string[] expected = {"6/9/88"};
 
-            Assert.AreEqual(expected, rewriter.ConvertDate(input));
+            CollectionAssert.AreEqual(expected, rewriter.ConvertDate(input));
         }
 
         [TestMethod]
         public void SingleFullNumericDate_FullDate5DigitYear_Test()
         {
             string input = "05/31/02006";
-            string expected = "5/31/06";
+            string[] expected = {"5/31/06"};
 
-            Assert.AreEqual(expected, rewriter.ConvertDate(input));
+            CollectionAssert.AreEqual(expected, rewriter.ConvertDate(input));
+        }
+
+        [TestMethod]
+        public void SingleFullNumericDate_3YearsFullComma_Test()
+        {
+            string input = "2005,2006,2007";
+            string[] expected = {"1/1/05","1/1/06","1/1/07"};
+
+            CollectionAssert.AreEqual(expected, rewriter.ConvertDate(input));
+        }
+
+        [TestMethod]
+        public void SingleFullNumericDate_2YearsFullDash_Test()
+        {
+            string input = "1986-1990";
+            string[] expected = { "1/1/86", "1/1/90" };
+
+            CollectionAssert.AreEqual(expected, rewriter.ConvertDate(input));
+        }
+
+        [TestMethod]
+        public void SingleFullNumericDate_2YearsFullSlash_Test()
+        {
+            string input = "1995/1996";
+            string[] expected = { "1/1/95", "1/1/96" };
+
+            CollectionAssert.AreEqual(expected, rewriter.ConvertDate(input));
+        }
+
+        [TestMethod]
+        public void SingleFullNumericDate_2YearsFullSlashSpace_Test()
+        {
+            string input = "1995/ 1996";
+            string[] expected = { "1/1/95", "1/1/96" };
+
+            CollectionAssert.AreEqual(expected, rewriter.ConvertDate(input));
+        }
+
+        [TestMethod]
+        public void SingleFullNumericDate_2YearsFullAmpersandSpace_Test()
+        {
+            string input = "2004 & 2006";
+            string[] expected = { "1/1/04", "1/1/06" };
+
+            CollectionAssert.AreEqual(expected, rewriter.ConvertDate(input));
+        }
+
+        [TestMethod]
+        public void SingleFullNumericDate_2YearsFullCommaSpace_Test()
+        {
+            string input = "1991,1992";
+            string[] expected = { "1/1/91", "1/1/92" };
+
+            CollectionAssert.AreEqual(expected, rewriter.ConvertDate(input));
+        }
+
+        [TestMethod]
+        public void SingleFullNumericDate_2YearsFull1DigitDash_Test()
+        {
+            string input = "2003-9";
+            string[] expected = { "1/1/03", "1/1/09" };
+
+            CollectionAssert.AreEqual(expected, rewriter.ConvertDate(input));
+        }
+
+        [TestMethod]
+        public void SingleFullNumericDate_MonthAlphaCommaFullYear_Test()
+        {
+            string input = "June, 2008";
+            string[] expected = { "6/1/08" };
+
+            CollectionAssert.AreEqual(expected, rewriter.ConvertDate(input));
+        }
+
+        [TestMethod]
+        public void SingleFullNumericDate_MonthAlphaCommaFullYears_Test()
+        {
+            string input = "June, 2008-September, 2009";
+            string[] expected = { "6/1/08", "9/1/09" };
+
+            CollectionAssert.AreEqual(expected, rewriter.ConvertDate(input));
+        }
+
+        [TestMethod]
+        public void SingleFullNumericDate_2YearsFullShortDash_Test()
+        {
+            string input = "2005-06";
+            string[] expected = { "1/1/05", "1/1/06" };
+
+            CollectionAssert.AreEqual(expected, rewriter.ConvertDate(input));
         }
     }
 }
